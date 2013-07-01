d5f6d67b0f4948093283b8ed8d4ca2a7f3419afe
Looton <lypwork@qq.com>
2013-07-01 18:40:55 +0800

    Rubbit

---
+++ src/com/looyp/typical40topic/RubbitTest.java
@@ -0,0 +1,25 @@
+/**
+ * @FileName    Topic01.java
+ * @Description 有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第四个月后每个月又生一对兔子，
+ *              假如兔子都不死，问每个月的兔子总数为多少？   
+ * @author      lyp3314@gmail.com
+ * @Time        
+ */
+
+//程序分析：   兔子的规律为数列1,1,2,3,5,8,13,21.... 
+package com.looyp.typical40topic;
+
+public class RubbitTest {
+	public static void main(String args[]) {
+		int i = 0;
+		for (i = 1; i <= 24; i++)
+			System.out.println("第" + i + "个月的兔子总数: " + f(i));
+	}
+
+	public static int f(int x) {
+		if (x == 1 || x == 2)
+			return 1;
+		else
+			return f(x - 1) + f(x - 2);
+	}
+}
